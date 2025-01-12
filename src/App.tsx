fd525d04e5b53a0801feaadd7fc9985fe0bd3a29
Carlos Afonso <carlos.flach15@gmail.com>
2025-01-12 18:06:12 -0300

    feat: Fetching orders

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import Home from './ui/Home';
 import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
 import CreateOrder from './features/order/CreateOrder';
-import Order from './features/order/Order';
+import Order, { loader as orderLoader} from './features/order/Order';
 import AppLayout from './ui/AppLayout';
-import PageNotFound from './ui/PageNotFound';
+import Error from './ui/Error';
 
 const router = createBrowserRouter([
 	{
 		element: <AppLayout />,
-		errorElement: <PageNotFound />,
+		errorElement: <Error />,
 
 		children: [
 			{
@@ -21,7 +21,7 @@ const router = createBrowserRouter([
 				path: '/menu',
 				element: <Menu />,
 				loader: menuLoader,
-				errorElement: <PageNotFound />,
+				errorElement: <Error />,
 			},
 			{
 				path: '/cart',
@@ -34,6 +34,8 @@ const router = createBrowserRouter([
 			{
 				path: '/order/:orderId',
 				element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
 			},
 		],
 	},
