07d64e6f4486792e3ef823d12fc597321d177a8c
Carlos Afonso <carlos.flach15@gmail.com>
2025-01-12 18:03:49 -0300

    feat: creates an search order component

---
+++ src/features/order/SearchOrder.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function SearchOrder() {
+	const [query, setQuery] = useState('');
+  const navigate = useNavigate()
+
+    function handleSubmit(e: React.SyntheticEvent) {
+      e.preventDefault()
+      if(!query) return
+      navigate(`/order/${query}`)
+    }
+	return (
+		<form onSubmit={handleSubmit}>
+			<input placeholder='Search order #' value={query} onChange={e => setQuery(e.target.value)} />
+		</form>
+	);
+}
