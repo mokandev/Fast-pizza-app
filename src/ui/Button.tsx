89f116419ab672f05fd9f7b70a79e82f5fa5b18f
Carlos Afonso <carlos.flach15@gmail.com>
2025-01-13 00:09:44 -0300

    feat: Styling the Cart

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -4,7 +4,7 @@ interface IButtonProps {
   children: string;
   disabled?: boolean;
   to?: string;
-  type: 'small' | 'primary';
+  type: 'small' | 'primary' | 'secondary';
 }
 
 export const Button: React.FC<IButtonProps> = ({
@@ -19,6 +19,8 @@ export const Button: React.FC<IButtonProps> = ({
   const styles = {
     primary: base + 'px-4 py-3 md:px-10 md:py-4',
     small: base + 'px-4 py-2 md:px-5 md:py-2.5 text-xs',
+    secondary:
+      'inline-block rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-all duration-300 hover:bg-stone-300 hover:text-stone-800 focus:text-stone-800 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-stone-500 px-4 py-2.5 md:px-10 md:py-3.5 ',
   };
 
   if (to) {
